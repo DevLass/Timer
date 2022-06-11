77696644b8ea5250a21fca67c2a0a9bf6ead9896
Nich <96788087+italicnich@users.noreply.github.com>
2022-06-11 17:21:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,110 @@ function count(){
     }
     document.getElementById('time').innerHTML = zerozero(hour)+':'+zerozero(min)+':'+zerozero(sec)
 }
+
+var hour_f
+var min_f
+var sec_f
+
+function collect_popup(){
+    var hour_inp = document.querySelector('#hours_input')
+    var min_inp  = document.querySelector('#min_input')
+    var sec_inp  = document.querySelector('#sec_input')
+
+    hour_f = hour_inp.value
+    min_f = min_inp.value
+    sec_f = sec_inp.value
+}
+
+
+function stopwatch(){
+    hour = hour_f
+    min = min_f
+    sec = sec_f
+
+    if( min == 0){
+        hour--
+        min = 60
+    }
+
+    if( sec == 0){
+        min--
+        sec = 60
+    }
+
+
+    restart_stopwatch();
+
+}
+
+function decrease_count(){
+    document.getElementById('time').innerHTML = zerozero(hour)+':'+zerozero(min)+':'+zerozero(sec)
+    sec--
+    if (sec == 0){
+        min--
+        sec = 60
+        if(min ==0){
+            hour--
+            min = 60
+        }
+    }
+}
+
+function restart_stopwatch(){
+    decrease_count()
+    interval = setInterval(decrease_count,1000)
+}
+
+function pomodoro(){
+    hour = 00
+    min = 25
+    sec = 00
+    if( min == 0){
+        hour--
+        min = 60
+    }
+
+    if( sec == 0){
+        min--
+        sec = 60
+    }
+
+    restart_stopwatch();
+
+    setTimeout(function(){
+
+        hour = 00
+        min = 5
+        sec = 00
+        if( min == 0){
+            hour--
+            min = 60
+        }
+    
+        if( sec == 0){
+            min--
+            sec = 60
+        }
+    
+        restart_stopwatch();
+
+    }, 1500000);
+    
+    setTimeout(function(){
+        pomodoro();
+
+    }, 1800000);
+    
+}
+
+const button = document.querySelector('#stopwatch_button')
+const popup = document.querySelector('.popup-wrapper')
+const close_button = document.querySelector('.popup-close')
+
+function active_popup(){
+    popup.style.display = 'block'
+}
+
+function close_popup(){
+    popup.style.display = 'none'
+}
